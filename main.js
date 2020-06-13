a6dd732b5ffd17a84c4e90114d546a64b8d96b0b
KietM <66890190+KietM@users.noreply.github.com>
2020-06-14 06:51:32 +0700

    DiscordBot

---
+++ main.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const obt = new Discord.Client();
+
+bot.on(ready", function (){
+               console.log('${bot.user.username} Is Online!`);
+});
+
+bot.login(process.env.token);
